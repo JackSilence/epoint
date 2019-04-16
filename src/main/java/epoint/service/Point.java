dbc58a43affb7b9ad99d3240b4974fd56dd35778
JackSilence <jacksilence@gmail.com>
2019-04-16 14:07:24 +0800

    調整libtesseract路徑

--- src/main/java/epoint/service/Point.java
+++ src/main/java/epoint/service/Point.java
@@ -59,8 +59,8 @@ public class Point implements IService {
 	static {
 		try {
 			System.out.println( "System.getProperty(\"java.library.path\"): " + System.getProperty( "java.library.path" ) );
-			System.load( "/app/.apt/usr/lib/x86_64-linux-gnu/libtesseract.so.4.0.0" );
-			System.load( "/app/.apt/usr/lib/x86_64-linux-gnu/liblept.so.5" );
+			System.load( "/app/vendor/tesseract-ocr/lib/libtesseract.so" );
+			System.load( "/app/vendor/tesseract-ocr/lib/liblept.so" );
 		} catch ( UnsatisfiedLinkError e ) {
 			System.err.println( "Native code library failed to load.\n" + e );
 
