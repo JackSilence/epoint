e1b2b0c0a345a5cf77c9c3da26daed1f5eb19061
JackSilence <jacksilence@gmail.com>
2019-04-16 14:00:44 +0800

    改用system load

--- src/main/java/epoint/service/Point.java
+++ src/main/java/epoint/service/Point.java
@@ -56,6 +56,17 @@ public class Point implements IService {
 	@Value( "${GOOGLE_CHROME_SHIM:}" )
 	private String bin;
 
+	static {
+		try {
+			System.out.println( "System.getProperty(\"java.library.path\"): " + System.getProperty( "java.library.path" ) );
+			System.load( "/app/.apt/usr/lib/x86_64-linux-gnu/libtesseract.so.4.0.0" );
+			System.load( "/app/.apt/usr/lib/x86_64-linux-gnu/liblept.so.5" );
+		} catch ( UnsatisfiedLinkError e ) {
+			System.err.println( "Native code library failed to load.\n" + e );
+
+		}
+	}
+
 	@Override
 	@Scheduled( cron = "0 0 11,18 * * *" )
 	public void exec() {
